74527ed9b2ad1e28de98d4c087d8c82acc6dca18
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-03-12 21:59:25 -0700

    change the url to fetch data

--- lib/fetchData/todoApi.ts
+++ lib/fetchData/todoApi.ts
@@ -1,14 +1,14 @@
 const URL: string = process.env.BASE_URL!;
 
 export const getTodo = async () => {
-  const res = await fetch(`${URL}api/todos`);
+  const res = await fetch(`api/todos`);
   const result = await res.json();
 
   return result;
 };
 
 export const postTodo = async (data: { description: string }) => {
-  const res = await fetch(`${URL}api/todos`, {
+  const res = await fetch(`api/todos`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
@@ -24,7 +24,7 @@ export const updateTodo = async ({
   id: string;
   data: { description: string };
 }) => {
-  const res = await fetch(`${URL}api/todos/${id}`, {
+  const res = await fetch(`api/todos/${id}`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
@@ -34,7 +34,7 @@ export const updateTodo = async ({
 };
 
 export const getSingleTodo = async (id: string) => {
-  const res = await fetch(`${URL}api/todos/${id}`, {
+  const res = await fetch(`api/todos/${id}`, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
   });
@@ -43,7 +43,7 @@ export const getSingleTodo = async (id: string) => {
 };
 
 export const deleteTodo = async (id: string) => {
-  const res = await fetch(`${URL}api/todos/${id}`, {
+  const res = await fetch(`api/todos/${id}`, {
     method: "DELETE",
     headers: { "Content-Type": "application/json" },
   });
